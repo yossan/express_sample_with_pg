6109f9ece23f3d99ee63bca947b231eebd02d01f
yossan <kosuke.yoshimoto@gmail.com>
2018-07-24 10:31:27 +0900

    Change the index route to be the same as employees

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,7 @@
 const express = require('express')
 const router = express.Router()
+const employees = require('./employees')
 
-router.get('/', function (req, res) {
-    res.send('Hi')
-})
+router.use('/', employees)
 
 module.exports = router
